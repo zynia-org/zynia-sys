fba5172ef3ef255d368d3f432c74d7c8f0d1e1ad
Frank Halasz <frank@zynia.org>
2023-07-07 11:05:39 -0700

    Updating osconfig.sh to add timezone set and remove chromium load plus several small fixes/improvements

--- osconfig.sh
+++ osconfig.sh
@@ -9,22 +9,29 @@ else
   SUDO=""
 fi
 
+# kick off sudo passwd request
+${SUDO} touch foo-$$
+${SUDO} rm -f foo-$$
+
 # Add new user
-read -p "Username?: " USERNAME
+read -p "Username for additional user?: " USERNAME
 if [ -n "${USERNAME}" ]
 then
-  ${SUDO} adduser ${USERNAME}
+  ${SUDO} adduser --gecos "" ${USERNAME}
   ${SUDO} usermod -aG sudo ${USERNAME}
-  ${SUDO} echo "${USERNAME} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/${USERNAME}
-  ${SUDO} passwd ${USERNAME} 
+  ${SUDO} echo "${USERNAME} ALL=(ALL) NOPASSWD:ALL" | ${SUDO} tee /etc/sudoers.d/${USERNAME} >/dev/null
 fi
 
 # resize partition4 on sd card to take up rest of space
-echo ", +" | ${SUDO} /sbin/sfdisk /dev/mmcblk1 -N4
+echo ", +" | ${SUDO} /sbin/sfdisk /dev/mmcblk1 -N4 --force
+${SUDO} /sbin/partprobe
 ${SUDO} /sbin/resize2fs /dev/mmcblk1p4
 
 # apt update
 ${SUDO} apt update
+
+# git
+${SUDO} apt install -y git
 
 # Firefox
 ${SUDO} DEBIAN_FRONTEND=noninteractive apt-get install libevent-dev libdbus-glib-1-dev -y
@@ -33,13 +40,16 @@ ${SUDO} dpkg -i firefox_105.0_202305_riscv64.deb
 rm firefox_105.0_202305_riscv64.deb
 
 # Chromium
-${SUDO} DEBIAN_FRONTEND=noninteractive apt-get install libre2-9 libminizip-dev -y
-wget https://github.com/starfive-tech/Debian/releases/download/v0.8.0-engineering-release-wayland/chromium-103.0.5060.114.deb
-${SUDO} dpkg -i chromium-103.0.5060.114.deb
-rm chromium-103.0.5060.114.deb
+# Skip Chromium for now since it doesn't really work on VF2 - FGH 2023-07-06
+#${SUDO} DEBIAN_FRONTEND=noninteractive apt-get install libre2-9 libminizip-dev -y
+#wget https://github.com/starfive-tech/Debian/releases/download/v0.8.0-engineering-release-wayland/chromium-103.0.5060.114.deb
+#${SUDO} dpkg -i chromium-103.0.5060.114.deb
+#rm chromium-103.0.5060.114.deb
+#export DISPLAY=":0"
+#chrome
 
-# git
-${SUDO} apt install git
+# set the timezone to west coast US
+${SUDO} timedatectl set-timezone America/Los_Angeles
 
-
-
+# reboot
+${SUDO} systemctl reboot
